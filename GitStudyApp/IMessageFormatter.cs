796506e71a38443c6b164ff4369be3b6fce0c7a7
Dmytro Kovalchuk <dmitrokovalchuk2612@gmail.com>
2020-06-22 09:11:10 +0300

    Create interface IMessageFormatter in 'develop' branch

---
+++ GitStudyApp/IMessageFormatter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitStudyApp
+{
+    public interface IMessageFormatter
+    {
+        string FormatMessage(string message);
+    }
+}
