22669fe5a070d3f667aa7ef2bde9952a7aa73968
Dmytro Kovalchuk <dmitrokovalchuk2612@gmail.com>
2020-06-22 12:49:22 +0300

    hello-world-line commented

--- GitStudyApp/Program.cs
+++ GitStudyApp/Program.cs
@@ -6,7 +6,7 @@ namespace GitStudyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
 
             Console.WriteLine("This code was added in 'develop' branch");
         }
