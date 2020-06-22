80b42e391b940aba800b35e2ea7e396e98bd6c24
Dmytro Kovalchuk <dmitrokovalchuk2612@gmail.com>
2020-06-22 09:23:18 +0300

    Create MessageFormatter that implements IMessageFormatter in 'feature' branch

---
+++ GitStudyApp/MessageFormatter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitStudyApp
+{
+    public class MessageFormatter : IMessageFormatter
+    {
+        public string FormatMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
